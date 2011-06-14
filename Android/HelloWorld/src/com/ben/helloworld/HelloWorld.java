74ab054381d200af645c884e76b0f3238a0e4acc
BenKwan <freebendy@gmail.com>
2011-06-14 10:57:07 +0800

    Add some test codes.

--- Android/HelloWorld/src/com/ben/helloworld/HelloWorld.java
+++ Android/HelloWorld/src/com/ben/helloworld/HelloWorld.java
@@ -1,13 +1,39 @@
 package com.ben.helloworld;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class HelloWorld extends Activity {
+
+    private static final String LOG_TAG = "HelloWorld";
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("1", "11");
+        map.put("2", "22");
+        Test test = new Test(map);
+        Log.v(LOG_TAG, "1: " + (String) test.getObject().get("1"));
+        map.put("1", "1111");
+        Log.v(LOG_TAG, "2: " + (String) test.getObject().get("1"));
+    }
+
+    public static class Test {
+        private Map<?, ?> mObj;
+        public Test(Map<?, ?> aObject) {
+            mObj = aObject;
+        }
+
+        Map<?, ?> getObject() {
+            return mObj;
+        }
     }
 }
